c23ec4c652c25e28acf9b688eaa906c2d7516033
AsyaKhabibova <asikan2014@ya.ru>
2020-12-16 16:59:44 +0500

    main

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
-import pygame
+import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode((576,1024)) #ширина и высота экрана
+clock = pygame.time.Clock()
 while True: #игровой цикл
     for event in pygame.event.get(): #ищет все события которые происходят прямо сейчас(движение мыш)
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
     pygame.display.update()
+    clock.tick(120)
