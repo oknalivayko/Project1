2433737b9d24c70d14e1cb5da7e4c549bc2a544d
oknalivayko <oknalivayko@miem.hse.ru>
2020-12-17 01:16:03 +0500

    main

--- 1.py
+++ 1.py
@@ -2,67 +2,78 @@ import pygame, sys, random
 def moving(): #функция непрерывного(для игрока) движения черной дыры
     screen.blit(black_hole,(x,700))
     screen.blit(black_hole,(x+432,700))
-
-def create_asteroid():
-    random_asteroid_position = random.choice(asteroid_height)
-    new_asteroid_down = asteroid_s.get_rect(midtop = (700,random_asteroid_position))
-    new_asteroid_top = asteroid_s.get_rect(midbottom = (700,random_asteroid_position-300))
-    return new_asteroid_down, new_asteroid_top
-
-def move_asteroids(asteroids):
-    for asteroid in asteroids:
-        asteroid.centerx -= 5
-    return asteroids
-
-def draw_asteroids(asteroids):
-    for asteroid in asteroids:
-        screen.blit(asteroid_s,asteroid) 
+def create_comet():
+    random_comet_pos = random.choice(comet_heiht)
+    new_comet = comet_draw.get_rect(center=(700,random_comet_pos))
+    return new_comet
+def move_comets(comets):
+    for comet in comets:
+        comet.centerx -= 5 
+    return(comets)
+def draw_comets(comets):
+    for comet in comets:
+        screen.blit(comet_draw,comet)
+def crash(comets):
+    for comet in comets:
+        if pers_rect.colliderect(comet):
+            return False
+    if pers_rect.top<=-100 or pers_rect.bottom>=700:
+        return False
+    return True
 
 pygame.init()
 
 x = 0
 gravity = 0.125 #создаем гравитацию
 pers_movement = 0 #движение персонажа
-
+game = True
 
 screen = pygame.display.set_mode((432,768)) #ширина и высота экрана
 
 clock = pygame.time.Clock()
 
 back = pygame.image.load("back.png").convert() #преобразует изобр в тип файла с которым легче работаь 
-
 black_hole = pygame.image.load("black_hole.png").convert()
 
 pers = pygame.image.load("pers.png").convert()
 pers_rect = pers.get_rect(center = (75,384)) #помещаем персонажа в "прямоугольник" и располагаем в центре экрана
-
-asteroid_s = pygame.image.load("asteroid.png")
-asteroid_list = []
+comet_draw = pygame.image.load("comet.png").convert()
+comet_list = []
 spawn = pygame.USEREVENT
 pygame.time.set_timer(spawn,1200)
-asteroid_height = [1500,200,300]
+comet_heiht = [100,200,400]
+
 while True: #игровой цикл
     for event in pygame.event.get(): #ищет все события которые происходят прямо сейчас(движение мыш)
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN: #проверяет была ли нажата кнопка на клавиатуре
-            if event.key == pygame.K_SPACE: #если нажали кнопку W
+            if event.key == pygame.K_SPACE and game == True: #если нажали кнопку space
                 pers_movement = 0
                 pers_movement -= 5
-        if event.type == spawn:
-            asteroid_list.extend(create_asteroid())
+            if event.key == pygame.K_SPACE and game == False:
+                game = True
+                comet_list.clear()
+                pers_movement = 0
+                pers_rect.center = (75,384)
+        if event.type == pygame.USEREVENT:
+            comet_list.append(create_comet())
+            
+        
 
     screen.blit(back,(0,0))
     
-    pers_movement += gravity
-    pers_rect.centery += pers_movement #перемещаем центр "прямоугольника" вместе с персом
-    screen.blit(pers,pers_rect)
+    if game:
+        pers_movement += gravity
+        pers_rect.centery += pers_movement #перемещаем центр "прямоугольника" вместе с персом
+        screen.blit(pers,pers_rect)
+        game = crash(comet_list)
 
-    asteroid_list = move_asteroids(asteroid_list)
-    draw_asteroids(asteroid_list)
+        comet_list = move_comets(comet_list)
+        draw_comets(comet_list)
 
-
+        crash(comet_list)
     x -= 1 
     moving()
     if x <= -432:
