c9f989d6d95aefe7d69a9a9e01ee2343581bd1ef
AsyaKhabibova <asikan2014@ya.ru>
2020-12-16 17:44:46 +0500

    main

--- 1.py
+++ 1.py
@@ -2,10 +2,14 @@ import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode((576,1024)) #ширина и высота экрана
 clock = pygame.time.Clock()
+back = pygame.image.load("back.png").convert() #преобразует изобр в тип файла с которым легче работаь 
+black_hole = pygame.image.load("black_hole.png").convert()
 while True: #игровой цикл
     for event in pygame.event.get(): #ищет все события которые происходят прямо сейчас(движение мыш)
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.blit(back,(0,0))
+    screen.blit(black_hole,(0,800))
     pygame.display.update()
     clock.tick(120)
