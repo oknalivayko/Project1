9627db012c1ca22a1f7be3a4f764595feb9029c3
AsyaKhabibova <asikan2014@ya.ru>
2020-12-16 16:47:41 +0500

    main

--- 1.py
+++ 1.py
@@ -1,2 +1,8 @@
-print('hello')
-print ("")
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((576,1024)) #ширина и высота экрана
+while True: #игровой цикл
+    for event in pygame.event.get(): #ищет все события которые происходят прямо сейчас(движение мыш)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
