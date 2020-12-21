53dadd94fcf585ee552f9271a26fe1ddafc90dae
oknalivayko <oknalivayko@miem.hse.ru>
2020-12-21 15:48:03 +0500

    main

--- tests.py
+++ tests.py
@@ -12,3 +12,9 @@ class Test_Among (unittest.TestCase):
         asteroid_list = []
         asteroid_list.append(asteroid_draw.get_rect(center=(700,500)))
         self.assertEqual (move_asteroids(asteroid_list),[asteroid_draw.get_rect(center=(695,500))])
+        asteroid_list = [asteroid_draw.get_rect(center=(700,500)),asteroid_draw.get_rect(center=(695,500))]
+        self.assertEqual (move_asteroids(asteroid_list), [asteroid_draw.get_rect(center=(695,500)),asteroid_draw.get_rect(center=(690,500))])
+        asteroid_list = []
+        self.assertEqual (move_asteroids(asteroid_list),[])
+        asteroid_list = [asteroid_draw.get_rect(center=(0,0)),asteroid_draw.get_rect(center=(3,100))]
+        self.assertEqual (move_asteroids(asteroid_list), [asteroid_draw.get_rect(center=(-5,0)),asteroid_draw.get_rect(center=(-2,100))])
