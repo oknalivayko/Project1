15d7215dbc863d02d164a7e78e3e49c1b2ea70c3
AsyaKhabibova <asikan2014@ya.ru>
2020-12-21 15:41:30 +0500

    main

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 from main import score_update
+from main import move_asteroids
 class Test_Among (unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual (score_update(2,3), 7)
+        self.assertEqual (score_update(2,3), 3)
+        
